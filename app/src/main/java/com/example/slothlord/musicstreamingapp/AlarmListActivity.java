b58d5c82dc33180bda48ba67393c8732b16d11ae
noah2296 <noah2296@gmail.com>
2017-11-06 12:13:44 -0600

    Added empty activities and classes.

---
+++ app/src/main/java/com/example/slothlord/musicstreamingapp/AlarmListActivity.java
@@ -0,0 +1,13 @@
+package com.example.slothlord.musicstreamingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_list);
+    }
+}
---
+++ app/src/main/java/com/example/slothlord/musicstreamingapp/AlarmListController.java
@@ -0,0 +1,8 @@
+package com.example.slothlord.musicstreamingapp;
+
+/**
+ * Created by slothlord on 11/6/17.
+ */
+
+public class AlarmListController {
+}
