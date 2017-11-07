1dc5a10eaf5e035204e7b0081027fea9c806800d
jperales64 <jaimeperales@rocketmail.com>
2017-11-07 14:23:58 -0600

    Started AlarmList Activity

--- app/src/main/java/com/example/slothlord/musicstreamingapp/AlarmListActivity.java
+++ app/src/main/java/com/example/slothlord/musicstreamingapp/AlarmListActivity.java
@@ -2,6 +2,11 @@ package com.example.slothlord.musicstreamingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+
+
 
 public class AlarmListActivity extends AppCompatActivity {
 
@@ -9,5 +14,8 @@ public class AlarmListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_list);
+
+        final ListView alarmListView = (ListView) findViewById(R.id.alarmList);
+        final Button newAlarm = (Button) findViewById(R.id.newAlarm);
     }
 }
