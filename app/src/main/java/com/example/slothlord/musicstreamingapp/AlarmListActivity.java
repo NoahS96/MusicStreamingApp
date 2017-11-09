cb97bad1e5d2dfba95ac24b002c969eab7c1b4f3
jperales64 <jaimeperales@rocketmail.com>
2017-11-09 15:55:56 -0600

    Removed CreateAlarmActivity and now have a timepicker open when NewAlarm button is pressed

--- app/src/main/java/com/example/slothlord/musicstreamingapp/AlarmListActivity.java
+++ app/src/main/java/com/example/slothlord/musicstreamingapp/AlarmListActivity.java
@@ -1,28 +1,59 @@
 package com.example.slothlord.musicstreamingapp;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TimePicker;
 
-
+import java.util.Calendar;
 
 
 public class AlarmListActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_list);
+        //Recycle View
+         recyclerView = (RecyclerView) findViewById(R.id.almRecView);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
-        final ListView alarmListView = (ListView) findViewById(R.id.alarmList);
+
         final Button newAlarm = (Button) findViewById(R.id.newAlarm);
     }
 
     public void newAlarm(View view){
-        Intent intent = new Intent(this, CreateAlarmActivity.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this, CreateAlarmActivity.class);
+        //startActivity(intent);
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(), "timePicker");
+    }
+
+    public static class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener{
+
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            return new TimePickerDialog(getActivity(), this, hour, minute, false);
+        }
+        @Override
+        public void onTimeSet(TimePicker timePicker, int i, int i1) {
+
+        }
     }
 }
+
