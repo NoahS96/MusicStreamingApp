f46c340eca18bb286271f8b4bc88bcc75cf92639
Michael Canas <michaelcanas08@yahoo.com>
2017-12-03 17:57:54 -0600

    header,item_row,ic,pics added

--- app/src/main/java/com/example/slothlord/musicstreamingapp/NewsfeedJava/NewsFeedActivity.java
+++ app/src/main/java/com/example/slothlord/musicstreamingapp/NewsfeedJava/NewsFeedActivity.java
@@ -1,6 +1,8 @@
 package com.example.slothlord.musicstreamingapp.NewsfeedJava;
 
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,6 +21,20 @@ import java.util.ArrayList;
 public class NewsFeedActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    String TITLES[] = {"News Feed", "Radios", "Media Player", "Alarm"};
+    int ICONS[] = {R.drawable.ic_news_feed, R.drawable.ic_radios, R.drawable.ic_media_player, R.drawable.ic_alarm};
+
+    String NAME = "Akash Bangad";
+    String EMAIL = "akash.bangad@android4devs.com";
+    int PROFILE = R.mipmap.ic_sexi_man;
+
+    RecyclerView mRecyclerView;                           // Declaring RecyclerView
+    RecyclerView.Adapter mAdapter;                        // Declaring Adapter For Recycler View
+    RecyclerView.LayoutManager mLayoutManager;            // Declaring Layout Manager as a linear layout manager
+    DrawerLayout Drawer;                                  // Declaring DrawerLayout
+
+    ActionBarDrawerToggle mDrawerToggle;
+
 
     private RecyclerView recyclerView;
     private CardAdapter adapter;
@@ -31,7 +48,49 @@ public class NewsFeedActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
         setSupportActionBar(toolbar);                   // Setting toolbar as the ActionBar with setSupportActionBar() call
-        toolbar.bringToFront();
+        toolbar.bringToFront();                         //did nothing
+
+        /*mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView); // Assigning the RecyclerView Object to the xml View
+
+        mRecyclerView.setHasFixedSize(true);                            // Letting the system know that the list objects are of fixed size
+
+        mAdapter = new MyAdapter(TITLES,ICONS,NAME,EMAIL,PROFILE);       // Creating the Adapter of MyAdapter class(which we are going to see in a bit)
+        // And passing the titles,icons,header view name, header view email,
+        // and header view profile picture
+
+        mRecyclerView.setAdapter(mAdapter);                              // Setting the adapter to RecyclerView
+
+        mLayoutManager = new LinearLayoutManager(this);                 // Creating a layout Manager
+
+        mRecyclerView.setLayoutManager(mLayoutManager);                 // Setting the layout Manager
+
+
+        Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);        // Drawer object Assigned to the view
+        mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.openDrawer,R.string.closeDrawer){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                // code here will execute once the drawer is opened( As I dont want anything happened whe drawer is
+                // open I am not going to put anything here)
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                // Code here will execute once drawer is closed
+            }
+
+
+
+        }; // Drawer Toggle Object Made
+        Drawer.setDrawerListener(mDrawerToggle); // Drawer Listener set to the Drawer toggle
+        mDrawerToggle.syncState();               // Finally we set the drawer toggle sync State
+
+    }*/
+
+
+        /*^^^Drawer Initializations^^^^*/
 
         recyclerView = (RecyclerView) findViewById(R.id.cardView);
 
@@ -45,7 +104,7 @@ public class NewsFeedActivity extends AppCompatActivity {
 
         retrieveNews();
 
-    }
+        }
 
     private void retrieveNews() {
         NewsCard n = new NewsCard("The 'Despacito' Story Continues", "The \"Despacito\" phenomenon continues with this morning's announcement of the 2018 Grammy nominations.", R.mipmap.card1bkg);
@@ -63,3 +122,4 @@ public class NewsFeedActivity extends AppCompatActivity {
         adapter.notifyDataSetChanged();
     }
 }
+
